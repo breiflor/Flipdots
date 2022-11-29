bf69db1f662136beb89ecc3ff308a650f9fbe58c
Florian Breitkopf <florian.breitkopf@tum.de>
2022-11-29 17:55:04 +0100

    added Image show capabilities

--- Image.py
+++ Image.py
@@ -1,8 +1,9 @@
 import numpy as np
+import cv2
 
 class Image:
 
-    def __init__(self):
+    def __init__(self,imagepath=None):
         self.data = np.eye(28,dtype=int) # creates eigen matrix
         print(self.data)
 
@@ -20,8 +21,14 @@ class Image:
         val = np.matmul((row.reshape(1,7)),(np.array([64,32,16,8,4,2,1]).reshape(7,1)))
         return val[0][0].item()
 
-    def visualize(self):
-        pass
+    def show(self,original=False,scale_to_px=500):
+        img  = np.zeros((28,28))
+        img = img+self.data
+        if(not original):
+            img = cv2.resize(img,(scale_to_px, scale_to_px), interpolation = cv2.INTER_AREA)
+        cv2.imshow("Image - scaled to "+str(scale_to_px)+" px", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     def load_txt(self):
         pass
@@ -35,3 +42,4 @@ class Image:
 if __name__ == "__main__":
     image = Image()
     print(image.getPanels(0))
+    image.show()
